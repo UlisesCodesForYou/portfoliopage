ffe95ae53c0c25fcf30c239188919a64e3dff118
Ulises Orozco <ulisesorozco6@gmail.com>
2022-11-15 21:00:51 -0800

    Fixed minor bugs

--- components/mainPage/mainPage.tsx
+++ components/mainPage/mainPage.tsx
@@ -17,7 +17,7 @@ export const MainPage = () => {
                                     className="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600"
                                     aria-label="toggle menu">
                                 <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
-                                    <path fill-rule="evenodd"
+                                    <path fillRule="evenodd"
                                           d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z">
                                     </path>
                                 </svg>
@@ -81,31 +81,32 @@ export const MainPage = () => {
                 {/*component*/}
                 <div className="mr-6 ml-6 bg-teal-600 shadow-xl shadow-teal-200-200 py-10 px-20 text-center">
                     <p className=" text-teal-600 ">
-                        <span className="text-4xl font-medium text-white">Technologies</span> <br/> <span className="text-lg text-white">Below are a few examples of the the tehcnologies I have used along with the sites/apps I created using them.</span>
+                        <span className="text-4xl font-medium text-white">Technologies</span> <br/> <span className="text-lg text-white">Below are a few examples of the the techcnologies I have used along with the sites/apps I created using them.</span>
                     </p>
                 </div>
                 <div className="mr-6 ml-6 px-3 md:lg:xl:px-40 border-t border-b py-20 bg-opacity-10">
                     <div className="grid grid-cols-1 md:lg:xl:grid-cols-3 group bg-gray-50 shadow-xl shadow-neutral-100 border">
                         <div className="p-10 flex flex-col items-center text-center group md:lg:xl:border-r md:lg:xl:border-b hover:bg-slate-50 cursor-pointer">
-                            <Image src="/Cutlery-Corner.png" width={700} height={700} alt="cutlery store"></Image>
-                            <p className="text-2xl font-medium mt-3 font-bold font-pageFont text-teal-600 ">
-                                REACT and REACT Router
-                            </p>
-                            <p className="text-xl font-medium  font-bold font-pageFont text-teal-600 mt-3">
+                            <p className="text-3xl font-medium font-bold font-pageFont text-teal-600 mt-3 mb-2">
                                 Cutlery Corner
                             </p>
+                            <Image src="/Cutlery-Corner.png" width={700} height={700} alt="cutlery store"></Image>
+                            <p className="text-xl font-medium mt-3 font-bold font-pageFont text-teal-600 ">
+                                REACT and REACT Router
+                            </p>
+
                             <p className="mt-2 text-sm font-pageFont text-teal-600 ">
                                 As a former chef, I thought it would be fun to finally create one. This is a static, cutlery website with functioning tabs.
                             </p>
                         </div>
 
                         <div className="p-10 flex flex-col items-center text-center group md:lg:xl:border-r md:lg:xl:border-b hover:bg-slate-50 cursor-pointer">
+                            <p className="text-3xl font-bold font-pageFont text-teal-600 mt-3 mb-2">
+                                Thriftster Mobile App
+                            </p>
                             <Image src="/Thriftster.png" width={200} height={200} alt="cutlery store"></Image>
                             <p className="text-2xl font-bold font-pageFont text-teal-600 mt-3">
                                 NEXT Js, Typescript, and Tailwind CSS.
-                            </p>
-                            <p className="text-xl  font-bold font-pageFont text-teal-600 mt-3">
-                                Thriftster Mobile App
                             </p>
                             <p className="mt-2 text-sm font-pageFont text-teal-600 ">
                                 This app was created after going to the thrift store and finding a hidden treasure at a low price. I also wanted to share the location of the store I found it in.
@@ -114,13 +115,12 @@ export const MainPage = () => {
                         </div>
 
                         <div className="p-10 flex flex-col items-center text-center group md:lg:xl:border-b hover:bg-slate-50 cursor-pointer">
+                            <p className="text-3xl font-bold font-pageFont text-teal-600 mt-3 mb-2">
+                                GAAD Foundation
+                            </p>
                             <Image src="/GAAD-Foundation.png" width={500} height={500} alt="cutlery store"></Image>
-
                             <p className="text-2xl font-bold font-pageFont text-teal-600 mt-3">
                                 NEXT Js, Typescript, and Mantine.
-                            </p>
-                            <p className="text-xl  font-bold font-pageFont text-teal-600 mt-3">
-                                GAAD Foundation
                             </p>
                             <p className="mt-2 text-sm font-pageFont text-teal-600 ">
                               I am currently volunteering in the reconstruction of this website.  GAAD foundtion was created he purpose of GAAD is to get everyone talking, thinking and learning about digital access and inclusion, and the more than One Billion people with disabilities/impairments.
