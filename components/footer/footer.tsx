9baa4a28d8e5987a6b2706897a6917cbb53b9ba1
Ulises Orozco <ulisesorozco6@gmail.com>
2022-11-17 14:46:34 -0800

    Made small changes in the layout.

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
             <footer className="text-center text-white bg-[#F05307]">
                 <div className="text-center p-4 bg-[#F05307]">
                     <Link href="https://github.com/UlisesCodesForYou/portfoliopage" target="_blank">
-                        <button className="bg-white text-black rounded-full font-pageFont tracking-wide py-3 px-7 text-xs hover:scale-110 duration-300 hover:bg-teal-600 hover:text-white">View this page's code on GitHub</button>
+                        <button className="bg-white text-black rounded-full font-pageFont tracking-wide py-3 px-7 text-xs hover:scale-110 duration-300 hover:bg-teal-600 hover:text-white">View this pages code on GitHub</button>
                     </Link>
                 </div>
                 <div className="container p-6 m-auto">
