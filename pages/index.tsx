fb564a9a1c509d082244e0af557e5bcdb8381b5d
Ulises Orozco <ulisesorozco6@gmail.com>
2022-11-29 13:38:15 -0800

    Added Favicon and tab name.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import MainPage from "../components/mainPage";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Ulises Orozco</title>
+        <link rel="icon" href="/Chef-hat.png" />
+      </Head>
       <MainPage />
     </>
   );
