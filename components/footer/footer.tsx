3a803cf95024801ade2b79939f8a249b73e6ae8c
Ulises Orozco <ulisesorozco6@gmail.com>
2022-12-07 20:49:01 -0800

    Added GitHub and LinkedIn logos along with links in the footer.

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -4,6 +4,14 @@ export const Footer = () => {
   return (
     <footer className="bg-teal-600 text-center text-white">
       <div className="p-4 text-center">
+        <Link
+          href="https://github.com/UlisesCodesForYou/portfoliopage"
+          target="_blank"
+        >
+          <button className="mb-4 rounded-full bg-white py-3 px-7 font-pageFont text-xs tracking-wide text-black duration-300 hover:scale-110 hover:bg-[#F05307] hover:text-white">
+            View this page&apos;s code on GitHub
+          </button>
+        </Link>
         <Image
           src="/Footer-Symbol.png"
           alt="Chef Hat"
@@ -11,26 +19,30 @@ export const Footer = () => {
           height={100}
           className="m-auto"
         />
-        <br />
-        <Link
-          href="https://github.com/UlisesCodesForYou/portfoliopage"
-          target="_blank"
-        >
-          <button className="rounded-full bg-white py-3 px-7 font-pageFont text-xs tracking-wide text-black duration-300 hover:scale-110 hover:bg-[#F05307] hover:text-white">
-            View this page&apos;s code on GitHub
-          </button>
-        </Link>
-        <p className="mt-2 mb-2 font-pageFont text-2xl">
-          And if you&apos;d like:
-        </p>
-        <Link href="https://github.com/UlisesCodesForYou" target="_blank">
-          <button className="rounded-full bg-white py-3 px-7 font-pageFont text-xs tracking-wide text-black duration-300 hover:scale-110 hover:bg-[#F05307] hover:text-white">
-            View my complete GitHub page
-          </button>
-        </Link>
+        <div className="mt-4 flex place-content-center space-x-5">
+          <Link href="https://github.com/UlisesCodesForYou" target="_blank">
+            <Image
+              src="/GitHub-Logo2.png"
+              alt="GitHub Logo"
+              width={50}
+              height={50}
+            />
+          </Link>
+          <Link
+            href="https://www.linkedin.com/in/ulises-orozco-dev/"
+            target="_blank"
+          >
+            <Image
+              src="/Linked-In-Logo.png"
+              alt="Linkedin Logo"
+              width={60}
+              height={0}
+            />
+          </Link>
+        </div>
       </div>
 
-      <div className="container m-auto p-6">
+      <div className="container m-auto -mt-5 p-6">
         <div className="text-center">
           <p>
             <span className="font-pageFont">
