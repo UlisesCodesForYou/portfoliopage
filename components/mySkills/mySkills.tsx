6e888ef53ad615b5d96d124de747b53bbe25db5c
Ulises Orozco <ulisesorozco6@gmail.com>
2022-12-12 10:34:33 -0800

    Adjusted Font and padding in smaller displays and removed excess files.

--- components/mySkills/mySkills.tsx
+++ components/mySkills/mySkills.tsx
@@ -1,26 +1,26 @@
 export const MySkills = () => {
   return (
-    <div className="sm:md:-mt-16 lg:mt-10">
+    <div className="p-8 sm:md:-mt-16 lg:mt-10">
       <div
         className="m-auto h-72 w-full bg-white py-10 text-center"
         id="myResume"
       >
-        <p className="">
+        <p className="p-10">
           <span className="mb-3 text-4xl font-medium text-teal-600">
             Key Skills:
           </span>{" "}
         </p>
-        <p className="mt-2 text-2xl text-teal-600 text-teal-600">
+        <p className="mt-2 text-center font-pageFont font-light text-teal-600 sm:text-xl">
           Please feel free to contact me if you would like to see my complete
           skill set and Resume.
         </p>
-        <p className="mt-2 text-2xl text-teal-600 text-teal-600">
+        <p className="mt-2 text-center font-pageFont font-light text-teal-600 sm:text-xl">
           In the meantime, here are a few of the skills and languages I have
           used:
         </p>
 
         <ul className="mt-5 mb-3 grid grid-cols-2 sm:grid-cols-2">
-          <div className="group list-disc flex-col font-pageFont text-teal-600 ">
+          <div className="group flex-col font-pageFont text-teal-600 ">
             <li>React</li>
             <li>React Router</li>
             <li>React Redux</li>
