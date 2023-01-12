71ddaa65e66b7ea5272a950d918512d402dc7fb6
Ulises Orozco <ulisesorozco6@gmail.com>
2023-01-11 18:02:27 -0800

    Removed excess folders.  Also, made minor adjustments to the hero.

--- components/Hero/hero.tsx
+++ components/Hero/hero.tsx
@@ -7,7 +7,7 @@ export const Hero = () => {
         className="mt-18 container mx-auto mt-10 flex flex-col-reverse space-y-6 px-6 py-10 lg:h-[32rem] lg:flex-row lg:items-center lg:py-16"
         id="home"
       >
-        <div className="w-full lg:w-1/2 ">
+        <div className="w-full lg:mt-20 lg:w-1/2">
           <div className="lg:max-w-lg ">
             <h1 className="sm:text md:text text-center text-center font-pageFont text-xl font-bold text-teal-600 lg:text-left lg:text-4xl">
               Hello there!
@@ -17,7 +17,7 @@ export const Hero = () => {
             </h2>
 
             <div className="mt-5 space-y-5 ">
-              <p className="sm:text -mx-2 text-center font-pageFont text-teal-600 md:text-left lg:text-left">
+              <p className="-mx-2 font-pageFont text-teal-600 sm:text-left md:text-left lg:text-left">
                 I&apos;m a former Chef turned web developer. I design and build
                 websites and apps. I believe that both the Culinary and Software
                 development industries have the same root value; they both deal
@@ -30,10 +30,10 @@ export const Hero = () => {
         <div className="flex h-96 w-full items-center justify-center lg:w-1/2 ">
           <Image
             className="mx-auto object-cover xl:mt-10 2xl:mt-10"
-            src="/Portfolio-Logo.png"
+            src="/My-Logo.png"
             alt="chef hat"
-            width={600}
-            height={600}
+            width={400}
+            height={400}
           />
         </div>
       </div>
