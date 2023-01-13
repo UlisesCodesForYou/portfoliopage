b4f685926704ca2916d11dd6d3a9f724e1b6fc5b
Ulises Orozco <ulisesorozco6@gmail.com>
2023-01-13 12:41:19 -0800

    Reformatted About me section.

--- pages/otherPage.tsx
+++ pages/otherPage.tsx
@@ -1,65 +1,56 @@
-/* This example requires Tailwind CSS v3.0+ */
-
+import Image from "next/image";
 import NavBar from "@components/NavBar";
+import Hero from "@components/Hero";
 
-const Hero = () => {
+const AboutMe = () => {
   return (
     <>
       <NavBar />
-      <div className="isolate bg-white">
-        <main>
-          <div className="relative px-6 lg:px-8">
-            <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
-              <div className="sm:mt mt-16 md:mt-0 lg:mt-0">
-                <div>
-                  <h1 className="text-4xl font-bold tracking-tight text-gray-600 sm:text-center sm:text-6xl">
-                    Hello! My name is
-                  </h1>
-                  <h1 className="text-4xl font-bold tracking-tight text-gray-600 sm:text-center sm:text-6xl">
-                    Ulises Orozco
-                  </h1>
-                  <p className="mt-6 text-lg leading-8 text-gray-600 sm:text-center">
-                    I&apos;m a former Chef turned web developer. I design and
-                    build websites and apps. I believe that both the Culinary
-                    and Software development industries have the same root
-                    value; they both deal with providing great user/guest
-                    experiences.
-                  </p>
-                </div>
-                <div className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
-                  <svg
-                    className="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
-                    viewBox="0 0 1155 678"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
-                      fillOpacity=".3"
-                      d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-                    />
-                    <defs>
-                      <linearGradient
-                        id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
-                        x1="1155.49"
-                        x2="-78.208"
-                        y1=".177"
-                        y2="474.645"
-                        gradientUnits="userSpaceOnUse"
-                      >
-                        <stop stopColor="#014F86" />
-                        <stop offset={1} stopColor="#014F86" />
-                      </linearGradient>
-                    </defs>
-                  </svg>
-                </div>
+      <Hero />
+      <section className="bg-white" id="aboutMe">
+        <div className="relative flex items-center py-5">
+          <div className="m-8 flex-grow border-t border-gray-400"></div>
+          <span className="mx-4 flex-shrink font-pageFont text-4xl text-gray-600">
+            About Me
+          </span>
+          <div className="m-8 flex-grow border-t border-gray-400"></div>
+        </div>
+        <div className="container mx-auto -mt-2 flex flex-col-reverse space-y-6 px-6 py-10 lg:h-[32rem] lg:flex-row lg:items-center  lg:py-16 ">
+          <div className="w-full lg:w-1/2">
+            <div className="sm::text text-center md:text-center lg:max-w-lg lg:text-left">
+              <div className="mt-8 space-y-5">
+                <p className="md:text text-md -mx-2 flex items-center font-pageFont text-[#343A40]">
+                  My journey to become a web developer started back in 2017.
+                  While working full-time in the restaurant industry, I was
+                  became aware of the similarities between coding and cooking.
+                  As time progressed, and when time allowed, I came to
+                  appreciate the correlation between both; the goal of providing
+                  a great user/guest experience. As time went on, I began to see
+                  each language and its properties as ingredients; allowing me
+                  more creative freedom.
+                </p>
+                <p className="md:text text-md -mx-2 flex items-center font-pageFont text-[#343A40]">
+                  Fast-forward to today, and I have completed a few websites and
+                  apps. I&apos;m currently a volunteer for the GAAD (Global
+                  Awareness Accessibility Day) Foundation in their site
+                  reconstruction.
+                </p>
               </div>
             </div>
           </div>
-        </main>
-      </div>
+          <div className="flex h-96 w-full items-center justify-center lg:w-1/2">
+            <Image
+              className="mx-auto h-full w-full rounded-lg object-cover shadow-2xl lg:max-w-2xl"
+              src="/My-photo.jpeg"
+              alt="bald guy with glasses"
+              width={500}
+              height={500}
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 };
 
-export default Hero;
+export default AboutMe;
