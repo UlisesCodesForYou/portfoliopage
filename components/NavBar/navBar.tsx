c25c98efc55503e8db4e5080bd44c9978dc2a940
Ulises Orozco <ulisesorozco6@gmail.com>
2023-01-21 11:32:51 -0800

    Updated NavBar Data output.

--- components/NavBar/navBar.tsx
+++ components/NavBar/navBar.tsx
@@ -1,8 +1,125 @@
 import Image from "next/image";
 import { Link } from "react-scroll/modules";
-import { useState } from "react";
 
 export const NavBar = () => {
+  const data = [
+    {
+      id: "home",
+      text: "Home",
+      to: "home",
+      spy: true,
+      smooth: true,
+      offset: -150,
+      mobileOffset: -150,
+      duration: 100,
+    },
+    {
+      id: "about_me",
+      text: "About Me",
+      to: "aboutMe",
+      spy: true,
+      smooth: true,
+      offset: -40,
+      mobileOffset: -150,
+      duration: 100,
+    },
+    {
+      id: "my_projects",
+      text: "My Projects",
+      to: "myProjects",
+      spy: true,
+      smooth: true,
+      offset: -80,
+      mobileOffset: -90,
+      duration: 100,
+    },
+    {
+      id: "my_skills",
+      text: "My Skills",
+      to: "myResume",
+      spy: true,
+      smooth: true,
+      offset: -20,
+      mobileOffset: -90,
+      duration: 100,
+    },
+  ];
+
+  const btnStuff = [
+    {
+      id: "button",
+      text: "Contact Me",
+      to: "sayHello",
+      spy: true,
+      smooth: true,
+      offset: -80,
+      mobileOffset: -100,
+      duration: 100,
+    },
+  ];
+
+  const buttonData = btnStuff.map((bts) => (
+    <button
+      className="btn rounded-full duration-300 hover:scale-110 hover:text-[#023020] lg:bg-white lg:text-black lg:hover:bg-emerald-300"
+      key={bts.id}
+    >
+      <Link
+        to={bts.to}
+        spy={bts.spy}
+        smooth={bts.smooth}
+        offset={bts.offset}
+        duration={bts.duration}
+      >
+        {bts.text}
+      </Link>
+    </button>
+  ));
+
+  const navBarData = data.map((el) => (
+    <li className="py-4 text-white duration-300 hover:scale-125" key={el.id}>
+      <Link
+        className="outline-offset- block rounded hover:cursor-pointer lg:hover:bg-[#012A4A]"
+        to={el.to}
+        spy={el.spy}
+        smooth={el.smooth}
+        offset={el.offset}
+        duration={el.duration}
+      >
+        {el.text}
+      </Link>
+    </li>
+  ));
+
+  const dropDownData = data.map((dd) => (
+    <li className="rounded py-4 text-white" key={dd.id}>
+      <Link
+        className="outline-offset- block rounded hover:cursor-pointer hover:bg-emerald-300 hover:text-black"
+        to={dd.to}
+        spy={dd.spy}
+        smooth={dd.smooth}
+        offset={dd.mobileOffset}
+        duration={dd.duration}
+      >
+        {dd.text}
+      </Link>
+    </li>
+  ));
+
+  const contactData = btnStuff.map((ctd) => (
+    <li className="rounded py-4 text-white" key={ctd.id}>
+      <Link
+        className="block rounded hover:cursor-pointer hover:bg-emerald-300 hover:text-black"
+        to={ctd.to}
+        spy={ctd.spy}
+        smooth={ctd.smooth}
+        offset={ctd.mobileOffset}
+        duration={ctd.duration}
+      >
+        {ctd.text}
+      </Link>
+    </li>
+  ));
+
   return (
     <>
       <nav className="fixed top-0 left-0 z-50 mb-3 flex w-full flex-wrap items-center justify-between bg-[#014F86] py-3 py-3 px-2 font-pageFont shadow-xl">
@@ -17,65 +134,8 @@ export const NavBar = () => {
         </div>
         <div className="navbar relative flex hidden w-full justify-between lg:static lg:block lg:w-auto lg:justify-start">
           <ul className="menu menu-horizontal items-center gap-x-8">
-            <li className="py-4 text-white duration-300 hover:scale-125">
-              <Link
-                className="outline-offset- block rounded hover:cursor-pointer lg:hover:bg-[#012A4A]"
-                to="home"
-                spy={true}
-                smooth={true}
-                offset={-150}
-                duration={100}
-              >
-                Home
-              </Link>
-            </li>
-            <li className="py-4 text-white duration-300 hover:scale-125">
-              <Link
-                className="outline-offset- block rounded hover:cursor-pointer lg:hover:bg-[#012A4A]"
-                to="aboutMe"
-                spy={true}
-                smooth={true}
-                offset={-40}
-                duration={100}
-              >
-                About Me
-              </Link>
-            </li>
-            <li className="py-4 text-white duration-300 hover:scale-125">
-              <Link
-                className="rounded hover:cursor-pointer lg:hover:bg-[#012A4A]"
-                to="myProjects"
-                spy={true}
-                smooth={true}
-                offset={-80}
-                duration={100}
-              >
-                My Projects
-              </Link>
-            </li>
-            <li className="py-4 text-white duration-300 hover:scale-125">
-              <Link
-                className="block rounded hover:cursor-pointer lg:hover:bg-[#012A4A]"
-                to="myResume"
-                spy={true}
-                smooth={true}
-                offset={-20}
-                duration={100}
-              >
-                My Skills
-              </Link>
-            </li>
-            <button className="btn rounded-full duration-300 hover:scale-110 hover:text-[#023020] lg:bg-white lg:text-black lg:hover:bg-emerald-300">
-              <Link
-                to="sayHello"
-                spy={true}
-                smooth={true}
-                offset={-80}
-                duration={100}
-              >
-                CONTACT ME
-              </Link>
-            </button>
+            {navBarData}
+            <div className="uppercase">{buttonData}</div>
           </ul>
         </div>
 
@@ -99,68 +159,10 @@ export const NavBar = () => {
           </label>
           <ul
             tabIndex={0}
-            className="dropdown-content menu rounded-box menu-compact mt-3 w-52 space-x-4 bg-[#012A4A] p-2 shadow"
+            className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-[#012A4A] p-2 shadow"
           >
-            <li className=" ml-4 rounded py-4 text-white">
-              <Link
-                className="outline-offset- block rounded hover:cursor-pointer hover:bg-emerald-300 hover:text-black"
-                to="home"
-                spy={true}
-                smooth={true}
-                offset={-150}
-                duration={100}
-              >
-                Home
-              </Link>
-            </li>
-            <li className="rounded py-4 text-white">
-              <Link
-                className="outline-offset- block rounded hover:cursor-pointer hover:bg-emerald-300 hover:text-black"
-                to="aboutMe"
-                spy={true}
-                smooth={true}
-                offset={-150}
-                duration={100}
-              >
-                About Me
-              </Link>
-            </li>
-            <li className="rounded py-4 text-white">
-              <Link
-                className="hover:cursor-pointer hover:bg-emerald-300 hover:text-black"
-                to="myProjects"
-                spy={true}
-                smooth={true}
-                offset={-90}
-                duration={100}
-              >
-                My Projects
-              </Link>
-            </li>
-            <li className="rounded py-4 text-white">
-              <Link
-                className="block rounded hover:cursor-pointer hover:bg-emerald-300 hover:text-black"
-                to="myResume"
-                spy={true}
-                smooth={true}
-                offset={-90}
-                duration={100}
-              >
-                My Skills
-              </Link>
-            </li>
-            <li className="rounded py-4 text-white">
-              <Link
-                className="block rounded hover:cursor-pointer hover:bg-emerald-300 hover:text-black"
-                to="sayHello"
-                spy={true}
-                smooth={true}
-                offset={-100}
-                duration={100}
-              >
-                Contact Me
-              </Link>
-            </li>
+            {dropDownData}
+            {contactData}
           </ul>
         </div>
       </nav>
